86fc6897952689c4a235ac35c1a2ec26960e3063
Nicholas Abad <nicoar9@gmail.com>
2021-03-30 12:19:39 -0500

    :sparkles:Feature: Login Page initial config and custom Widget for input

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,101 @@
+import 'package:chat_app/widgets/custom_input.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _Logo(),
+              _Form(),
+              _Labels(),
+              Text(
+                'Terms and conditions of use',
+                style: TextStyle(fontWeight: FontWeight.w200),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 50),
+        width: 170,
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage('assets/tag-logo.png'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Messenger',
+              style: TextStyle(fontSize: 30),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(),
+          CustomInput(),
+
+          // RaisedButton(
+          //   onPressed: () {},
+          // )
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Text(
+          'Dont have an Account?',
+          style: TextStyle(
+              color: Colors.black54, fontSize: 15, fontWeight: FontWeight.w300),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Create an Account Now!',
+          style: TextStyle(
+              color: Colors.blue[600],
+              fontSize: 18,
+              fontWeight: FontWeight.bold),
+        ),
+      ],
+    ));
   }
 }
