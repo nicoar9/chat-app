6774ce46105600a27f87e81d998cbecd1908dacf
Nicholas <nicoar9@gmail.com>
2021-03-31 12:20:01 -0500

    :sparkles:Feature: Adds initial config for users page and initial User Class

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chat App',
-      initialRoute: 'login',
+      initialRoute: 'users',
       routes: appRoutes,
     );
   }
